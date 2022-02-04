7c1b397df47ea59e9b19ba2fa3302501dd1bf0d7
Thomas701 <tho.dupois@laposte.net>
2022-02-04 13:29:13 +0100

    ajout Thomas regarde

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 int main(int argc, char ** argv)
 {
   printf("Hello World ! \n");
+  printf("TIHBAULT REGAAARDE!!!!");
   printf("Thibault, j'espère que tu pourra voir ce message\n");
   return EXIT_SUCCESS;
 }

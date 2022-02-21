64e8afe8b6996bab76bf65eb400b8807043c32ab
Thomas701 <tho.dupois@laposte.net>
2022-02-21 09:38:37 +0100

    recuperation thibault

--- main.c
+++ main.c
@@ -13,9 +13,43 @@ int main(int argc, char ** argv)
 {
   SDL_version np;
   SDL_VERSION(&np);
-  printf("Bienvenue sur la SDL %d %d %d\n", np.major, np.minor, np.patch);
+  printf("Bienvenue sur_ la SDL %d %d %d\n", np.major, np.minor, np.patch);
+  SDL_Window *window = NULL;
+  SDL_Renderer *renderer = NULL;
+  SDL_Color orange = {255, 127, 40, 255};
+  SDL_Rect rect = {100, 100, 100, 100};
+  SDL_RenderDrawRect(renderer, &rect);
+  int statut = EXIT_FAILURE;
 
-  char ** pileJ1 = createStack('b');
+  if(0 != SDL_Init(SDL_INIT_VIDEO))
+  {
+    fprintf(stderr, "Erreur SDL_Init : %s", SDL_GetError());
+    goto Quit;
+  }
+  window = SDL_CreateWindow("SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+  if(NULL == window)
+  {
+    fprintf(stderr, "Erreur SDL_CreateWindow : %s", SDL_GetError());
+    goto Quit;
+  }
+  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  if(NULL == renderer)
+  {
+    fprintf(stderr, "Erreur SDL_CreateRenderer : %s", SDL_GetError());
+    goto Quit;
+  }
+  if(0 != SDL_SetRenderDrawColor(renderer, orange.r, orange.g, orange.b, orange.a))
+  {
+    fprintf(stderr, "Erreur SDL_SetRenderDrawColor : %s", SDL_GetError());
+    goto Quit;
+  }
+  if(0 != SDL_RenderClear(renderer))
+  {
+    fprintf(stderr, "Erreur SDL_SetRenderDrawColor : %s", SDL_GetError());
+    goto Quit;
+  }
+  SDL_SetWindowTitle(window, "Gobble");
+  /*char ** pileJ1 = createStack('b');
   char ** pileJ2 = createStack('n');
   char *** map3D = createMap();
   char ** map2D = createMap2D();
@@ -41,9 +75,16 @@ int main(int argc, char ** argv)
   freeMap(map3D);
   freeMap2D(map2D);
   freeStack(pileJ1);
-  freeStack(pileJ2);
-
-  
-  
-  return EXIT_SUCCESS;
+  freeStack(pileJ2);*/
+  statut = EXIT_SUCCESS;
+  SDL_Delay(3000);
+  SDL_RenderPresent(renderer);
+  SDL_Delay(3000);
+Quit:
+  if(NULL != window)
+    SDL_DestroyWindow(window);
+  if(NULL != renderer)
+    SDL_DestroyRenderer(renderer);
+  SDL_Quit();
+  return statut;
 }

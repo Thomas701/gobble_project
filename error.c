ed9136d36df96bf30230e35603a88f2a9116071d
Thibault Villepreux <thibault.villepreux@gmail.com>
2022-03-29 21:56:11 +0200

    recup sniif

--- error.c
+++ error.c
@@ -1,22 +1,52 @@
-#include "fonction.h"
+/**
+ * \file error.c
+ * 
+ * \brief Fichier contenant les codes des fonctions d'erreurs (affiches messages et quitte le programme).
+ * 
+ * \author VILLEPREUX Thibault
+ */
 
-/*Fonctions d'erreur pour eviter la répétition :
-  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-
-void errorInCreatePile() 
-{
+/**
+ * \fn void errorInCreatePile()
+ * \brief Affiche erreur et quitte le programme (appeler si erreur lors de la création d'une pile).
+ * 
+ * \param[in] void : Ne prend aucun paramètre.
+ * 
+ * \return void: aucun retour.
+ * 
+ * \author VILLEPREUX Thibault
+ */
+void errorInCreatePile() {
     perror("ERROR ALLOCATION MEMORY in createPile\n");
     exit(EXIT_FAILURE);
 }
 
-void errorInCreate3D() 
-{
+/**
+ * \fn void errorInCreate3D()
+ * \brief Affiche erreur et quitte le programme (appeler si erreur lors de la création du plateau de jeu detaillé).
+ * 
+ * \param[in] void : Ne prend aucun paramètre.
+ * 
+ * \return void: aucun retour.
+ * 
+ * \author VILLEPREUX Thibault
+ */
+void errorInCreate3D() {
     perror("ERROR ALLOCATION MEMORY in createMap\n");
     exit(EXIT_FAILURE);
 }
 
-void errorInCreate2D() 
-{
+/**
+ * \fn void errorInCreate2D() 
+ * \brief Affiche erreur et quitte le programme (appeler si erreur lors de la création du plateau de jeu non detaillé).
+ * 
+ * \param[in] void : Ne prend aucun paramètre.
+ * 
+ * \return void: aucun retour.
+ * 
+ * \author VILLEPREUX Thibault
+ */
+void errorInCreate2D() {
     perror("ERROR ALLOCATION MEMORY in createMap2D\n");
     exit(EXIT_FAILURE);
 }
